215f64b5c425c394998de5a3b339471f23671aac
Ritik Puranik <ritikpuranik28@gmail.com>
2025-06-25 17:06:43 +0530

    nike

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div>Test</div>
+    <section id="home" className="w-full flex xl:flex-row flex-col justify-center 
+    gap-10 min-h-screen max-container ">
+      <div className='relative xl:w-2/5 flex flex-col justify-center items-start
+      w-full max-xl:padding-x pt-28'>
+        <p>Our Summer Collection</p>
+        <h1>
+          <span>The New Arrival</span>
+          <br/>
+          <span>Nike</span>
+          </h1>
+          <p>Discover stylish Nike arrivals, quality comfort, and innovation for your active life.</p>
+      </div>
+    </section>
   )
 }
 
